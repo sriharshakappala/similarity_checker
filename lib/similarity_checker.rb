34c43cefef12798f32a740125acb7565f7a27d04
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-01-10 15:36:55 +0530

    Clean up

--- lib/similarity_checker.rb
+++ lib/similarity_checker.rb
@@ -2,7 +2,7 @@ require "similarity_checker/version"
 
 module SimilarityChecker
   def self.get_similarity_index string_one, string_two
-  	string_one.downcase!
+    string_one.downcase!
 		string_two.downcase!
   	pairs_one = (0..string_one.length-2).collect {|i| string_one[i,2]}.reject { |pair| pair.include? " " }
   	pairs_two = (0..string_two.length-2).collect {|i| string_two[i,2]}.reject { |pair| pair.include? " " }
